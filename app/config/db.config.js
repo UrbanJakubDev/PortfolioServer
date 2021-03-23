5afb8453203e9c9c422a1d4466717721078d3def
jakub.urban <jakub.73@hotmail.com>
2021-03-23 09:38:40 +0100

    Change DB to Live

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,4 +1,4 @@
 module.exports = {
-  url: "mongodb://localhost:27017/bezkoder_db"
-  //url: `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_CLUSTER}/${process.env.DB_NAME}?retryWrites=true&w=majority`
-};
+  //url: "mongodb://localhost:27017/bezkoder_db"
+  url: `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_CLUSTER}/${process.env.DB_NAME}?retryWrites=true&w=majority`
+}
